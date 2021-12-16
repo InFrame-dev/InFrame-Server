3b004c902659b0a085b2014be4d2e13e4ace116b
민지 <71913069+Minj02@users.noreply.github.com>
2021-12-16 23:13:56 +0900

    findPassword

--- routes/users.js
+++ routes/users.js
@@ -126,6 +126,95 @@ router.post("/mail", async (req, res, next) => {
   }
 });
 
+
+router.post('/findPassword', async (req, res, next) => {
+  const email  = req.body.email;
+  const data = await User.findAll({
+    where: {
+      email: req.body.email,
+    }
+  });
+  
+  var generateRandom = function (min, max) {
+    var ranNum = Math.floor(Math.random()*(max-min+1)) + min;
+    return ranNum;
+  }
+  const number = generateRandom(111111,999999);
+  let transport = nodemailer.createTransport({
+
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PW,
+    },
+  });
+
+  // email 내용
+  let mailOptions = {
+    from: process.env.EMAIL,
+    to: req.body.email,
+    subject: "[InFrame] 회원가입 이메일 확인 절차입니다.",
+    html: "<p>아래 인증번호를 확인하고 입력해주세요!</p>" + number,
+
+  };
+
+  // email 전송
+  transport.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    User.update({code: number}, {where: {email: req.body.email}})
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+     console.error(err);
+    });
+
+    console.log(info);
+    console.log("send mail success!");
+  });
+  //res.redirect("/newpassword");
+});
+
+
+router.post('/code', async (req, res, next) => {
+  console.log(req.body);
+  if (
+      req.body.code 
+  ){
+    const data = await User.findAll({
+      where: {
+        email: req.session.email,
+      },
+    });    
+      if(req.body.code == data[0].code){
+        console.log("코드 확인 성공");
+          res.redirect("/");
+          //res.redirect("/newPassword");
+      }else{
+          console.log("코드가 잘못입력되었습니다");
+          //res.redirect("/mail");
+      }
+  } else {
+      res.send({ msg: "불완전한 데이터" });
+  }
+});
+
+router.post('/newPassword', async (req, res, next) => {
+  console.log(req.body);
+  const encryptedPassowrd = bcrypt.hashSync(req.body.password, 10);
+  User.update({password: encryptedPassowrd}, {where: {email: req.session.email}})
+  .then(result => {
+     res.json(result);
+  })
+  .catch(err => {
+     console.error(err);
+  });
+});
+
 router.get('/logout', (req, res) => {
   req.session.destroy();
   res.redirect("/");
